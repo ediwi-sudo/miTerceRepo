05e492c18c6f2665aea1551a2f293607f8780bb5
Edison Williams <edison.wica@gmail.com>
2024-11-30 23:16:15 -0500

    Update app.py

--- API/app.py
+++ API/app.py
@@ -10,6 +10,7 @@ def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
     return rows
 
+
 @app.get("/superheroesDC")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
